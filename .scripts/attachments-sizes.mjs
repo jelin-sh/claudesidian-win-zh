fbc9b8d1bba6d882f3f108eb85ad43d58b8a5a8b
jelin <jelin-sh@outlook.com>
2026-01-17 14:05:00 +0800

    fix: 修复代码格式问题

--- .scripts/attachments-sizes.mjs
+++ .scripts/attachments-sizes.mjs
@@ -31,8 +31,9 @@ function getFileSize(filePath) {
 
   if (size < 1024) return `${size} B`
   if (size < 1024 * 1024) return `${(size / 1024).toFixed(2)} KB`
-  if (size < 1024 * 1024 * 1024)
+  if (size < 1024 * 1024 * 1024){
     return `${(size / (1024 * 1024)).toFixed(2)} MB`
+  }
   return `${(size / (1024 * 1024 * 1024)).toFixed(2)} GB`
 }
 
