34ea510e72a8757ae3350c73b78956171339ca8f
jelin <jelin-sh@outlook.com>
2026-01-17 14:06:22 +0800

    fix: 修复代码格式问题，调整条件判断的括号位置

--- .scripts/attachments-sizes.mjs
+++ .scripts/attachments-sizes.mjs
@@ -31,7 +31,7 @@ function getFileSize(filePath) {
 
   if (size < 1024) return `${size} B`
   if (size < 1024 * 1024) return `${(size / 1024).toFixed(2)} KB`
-  if (size < 1024 * 1024 * 1024){
+  if (size < 1024 * 1024 * 1024) {
     return `${(size / (1024 * 1024)).toFixed(2)} MB`
   }
   return `${(size / (1024 * 1024 * 1024)).toFixed(2)} GB`
